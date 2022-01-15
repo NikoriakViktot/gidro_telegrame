c1aa2366317bc5025829d4f0a36911629df63461
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2022-01-15 22:56:02 +0200

    index_html

--- selenium_gidro/selenium_gidro.py
+++ selenium_gidro/selenium_gidro.py
@@ -9,7 +9,6 @@ from selenium.webdriver.chrome.service import Service
 from telegrame_save.request_index import request_index
 
 
-
 def get_auth_header(user, password):
     b64 = "Basic " + base64.b64encode('{}:{}'.format(user, password).encode('utf-8')).decode('utf-8')
     return {"Authorization": b64}
@@ -24,31 +23,31 @@ driver.execute_cdp_cmd("Network.setExtraHTTPHeaders",
 
 driver.get('http://gcst.meteo.gov.ua/armua/sino/index.phtml')
 
-time.sleep(5)
+time.sleep(2)
 
 submenu =driver.find_element(by=By.CLASS_NAME,value='submenu').find_element(by=By.XPATH, value='/html/body/table/tbody/tr/td[1]/a[10]').click()
 
-time.sleep(5)
+time.sleep(2)
 
-spusok_indexiv = '42256 42130 42249 42136 42137 42140 42148 42191 42187 42194 42198 42202 '
 for value in request_index():
     index = value[0]
-    print(index)
+    print(type(index))
+    time.sleep(0.5)
     forma_index = driver.find_element(by=By.CLASS_NAME, value='t1').send_keys(index)
-    time.sleep(2)
+    time.sleep(0.5)
     data_time = driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td/font/input[2]').clear()
-    time.sleep(1)
+    time.sleep(0.5)
     data_time_send = driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td/font/input[2]').send_keys(datetime.date.today().strftime("%Y-%m-%d")+ ' ' + '09:00:36')
-    time.sleep(2)
+    time.sleep(0.5)
     input_post = driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr/td[2]/form/table/tbody/tr[1]/td/input[2]').click()
-    time.sleep(2)
+    time.sleep(0.5)
     file_object = open(f'../telegrame_save/data_html/{index}.html', "w", encoding=('koi8-u'))
     html = driver.page_source
     file_object.write(html)
     file_object.close()
     submenu = driver.find_element(by=By.CLASS_NAME, value='submenu').find_element(by=By.XPATH,
                                                                                   value='/html/body/table/tbody/tr/td[1]/a[10]').click()
-    time.sleep(2)
+    time.sleep(5)
 
 driver.close()
 driver.quit()
--- telegrame_save/save_db.py
+++ telegrame_save/save_db.py
@@ -1,12 +1,41 @@
 import re
 from bs4 import BeautifulSoup
+import sqlite3 as sq
+from telegrame_save.request_index import request_index
 
-def open_html():
-        with open(f'\data_html\data.html', 'r', encoding='koi8-u') as file:
+def open_html(index):
+        with open(f'../telegrame_save/data_html/{index}.html', 'r', encoding='koi8-u') as file:
             r = file.read()
             soup = BeautifulSoup(r, "lxml")
             d = soup.find_all('pre')
             s = ['='.join(i) for i in d]
-            telegrams = [re.sub(("\s+"), " ", i) for i in s]
-            return telegrams
+            telegram = [re.sub(("\s+"), " ", i) for i in s]
+            print(telegram)
+            return telegram
 
+
+def pars_telegram():
+    telegram_html = open_html()
+    date_telegram = [i[26:32] for i in telegram_html]
+    print(date_telegram)
+
+
+
+
+
+def request_index():
+    with sq.connect('../gauges_telegrame.db') as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM index_gauges")
+        index = [x[0] for x in cur.fetchall()]
+        for value in index:
+            INDEX = []
+            INDEX.append(value)
+            yield INDEX
+
+
+
+for i in request_index():
+    open_html(i[0])
+
+pars_telegram()
