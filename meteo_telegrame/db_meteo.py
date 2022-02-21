e502a552994f48816865c714c5da7ea09aabf2ee
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2022-02-21 11:46:40 +0200

    create meteo db

---
+++ meteo_telegrame/db_meteo.py
@@ -0,0 +1,44 @@
+
+import sqlite3 as sq
+
+def index_meteo():
+    with open('index_meteo.txt', 'r')as f:
+        file = f.read()
+        index_gagues = []
+        for x in file.split():
+            index_gagues.append(x)
+        return index_gagues
+
+
+def create_db_meteo():
+    with sq.connect('../meteo_telegrame.db') as con:
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE  index_meteo
+                        (index_meteo TEXT NOT NULL)
+                         ''')
+        con.commit()
+        for i in index_meteo():
+            index_i = int(i)
+            print(index_i)
+            cur.execute(f'''CREATE TABLE  '{index_i}'
+                   (date DATA,
+                   meteo_telegrame TEXT NOT NULL)''')
+            con.commit()
+
+
+
+def save_db():
+    with sq.connect('../meteo_telegrame.db') as con:
+        cur = con.cursor()
+        for i in index_meteo():
+            data = i
+            print(data)
+            cur.execute(''' INSERT INTO index_meteo
+                        (index_meteo)
+                        VALUES(?)''', (data,))
+            con.commit()
+
+
+
+create_db_meteo()
+save_db()
---
+++ meteo_telegrame/save_db_meteo.py
@@ -0,0 +1,44 @@
+import re
+from bs4 import BeautifulSoup
+import os
+import sqlite3 as sq
+from telegrame_save.request_index import request_index
+
+def open_html(index):
+        with open(f'../telegrame_save/data_html/{index}.html', 'r', encoding='koi8-u') as file:
+            r = file.read()
+            soup = BeautifulSoup(r, "lxml")
+            d = soup.find_all('pre')
+            s = ['='.join(i) for i in d]
+            telegram = [re.sub(("\s+"), " ", i) for i in s]
+
+            for x in telegram:
+                try:
+                    yield x[0:19], x[20:]
+
+                except TypeError:
+                    yield None
+
+
+
+def save_db():
+    with sq.connect('../gauges_telegrame.db') as con:
+        cur = con.cursor()
+        for i in request_index():
+            index = i[0]
+            for s in open_html(index):
+                date = s[0]
+                telegram = s[1]
+                cur.execute(f'''insert INTO '{index}'
+                            (date, gauges_telegrame)
+                            VALUES(?,?) ''', (date, telegram))
+                con.commit()
+            os.remove(f'../telegrame_save/data_html/{index}.html')
+
+save_db()
+
+
+
+
+
+
